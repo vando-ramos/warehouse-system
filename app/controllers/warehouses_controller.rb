02db38cd90e9a82abfff2b1e66fe282bda0edc98
vando <vandorramos@gmail.com>
2024-10-09 00:30:30 -0300

    User registers a warehouse successfully

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -8,6 +8,16 @@ class WarehousesController < ApplicationController
     @warehouse = Warehouse.new
   end
 
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+
+    if @warehouse.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def warehouse_params
--- spec/system/user_registers_warehouse_spec.rb
+++ spec/system/user_registers_warehouse_spec.rb
@@ -18,4 +18,27 @@ describe 'User registers a warehouse' do
     expect(page).to have_field('Description')
     expect(page).to have_button('Create Warehouse')
   end
+
+  it 'successfully' do
+    # arrange
+
+    # act
+    visit(root_path)
+    click_on('Register Warehouse')
+    fill_in 'Name', with: 'Aeroporto SP'
+    fill_in 'Code', with: 'GRU'
+    fill_in 'City', with: 'Guarulhos'
+    fill_in 'Area', with: '13000000'
+    fill_in 'Address', with: 'Rodovia Hélio Smidt, s/n - Cumbica'
+    fill_in 'CEP', with: '07060-100'
+    fill_in 'Description', with: 'Cargas internacionai'
+    click_on('Create Warehouse')
+
+    # assert
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content('Aeroporto SP')
+    expect(page).to have_content('GRU')
+    expect(page).to have_content('Guarulhos')
+    expect(page).to have_content('13000000 m²')
+  end
 end

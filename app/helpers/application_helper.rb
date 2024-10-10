ad8017af42e1ec0265adac0d5aa22ca748774868
vando <vandorramos@gmail.com>
2024-10-10 00:52:54 -0300

    Create helper to display form fields errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def error_message_for(field)
+    return unless @warehouse.errors[field].any?
+
+    content_tag(:div, @warehouse.errors.full_messages_for(field).first, class: 'field-error')
+  end
 end

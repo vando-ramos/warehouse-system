1f27880246e5f1edbb76b718b235ab647378d4e5
vando <vandorramos@gmail.com>
2024-10-10 12:53:58 -0300

    User edits a warehouse and keeps the required fields

--- spec/system/user_edits_warehouse_spec.rb
+++ spec/system/user_edits_warehouse_spec.rb
@@ -38,7 +38,7 @@ describe 'User edits a warehouse' do
     fill_in 'Address', with: 'Rodovia Santos Dumont, km 66'
     fill_in 'CEP', with: '13052-000'
     fill_in 'Description', with: 'Cargas nacionais'
-    click_on ('Update Warehouse')
+    click_on('Update Warehouse')
 
     # assert
     expect(current_path).to eq(warehouse_path(warehouse.id))
@@ -49,5 +49,27 @@ describe 'User edits a warehouse' do
     expect(page).to have_content('3600000 m²')
     expect(page).to have_content('Rodovia Santos Dumont, km 66 - CEP: 13052-000')
     expect(page).to have_content('Cargas nacionais')
+  end
+
+  it 'and keeps the required fields' do
+    # arrange
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 13_000_000,
+                                  address: 'Rodovia Hélio Smidt, s/n - Cumbica', cep: '07060-100', description: 'Cargas internacionais')
+
+    # act
+    visit(root_path)
+    click_on('Aeroporto SP')
+    click_on('Edit')
+    fill_in 'Code', with: ''
+    fill_in 'Area', with: ''
+    fill_in 'CEP', with: ''
+    click_on('Update Warehouse')
+
+    # assert
+    expect(page).to have_content('Unable to update warehouse!')
+    expect(page).to have_field('Name', with: 'Aeroporto SP')
+    expect(page).to have_field('City', with: 'Guarulhos')
+    expect(page).to have_field('Address', with: 'Rodovia Hélio Smidt, s/n - Cumbica')
+    expect(page).to have_field('Description', with: 'Cargas internacionais')
   end
 end

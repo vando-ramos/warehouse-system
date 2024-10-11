b49942ed829931a120e0fdd598e2f8675694680c
vando <vandorramos@gmail.com>
2024-10-11 15:36:04 -0300

    User deletes a supplier and does not delete other suppliers

--- spec/system/suppliers/user_deletes_suppliers_spec.rb
+++ spec/system/suppliers/user_deletes_suppliers_spec.rb
@@ -20,4 +20,25 @@ describe 'User deletes a supplier' do
     expect(page).not_to have_content('TechInd')
     expect(page).not_to have_content('Curitiba - PR')
   end
+
+  it 'and does not delete other suppliers' do
+    # arrange
+    supplier = Supplier.create!(corporate_name: 'Comercial Alimentos S.A.', brand_name: 'SuperFood', registration_number: '12.345.678/0001-90',
+                                address: 'Rua das Palmeiras, 123', city: 'São Paulo', state: 'SP', email: 'contato@superfood.com.br')
+
+    supplier = Supplier.create!(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+    # act
+    visit(root_path)
+    click_on('Suppliers')
+    click_on('SuperFood')
+    click_on('Delete')
+
+    # assert
+    expect(current_path).to eq(suppliers_path)
+    expect(page).to have_content('Supplier successfully deleted!')
+    expect(page).not_to have_content('SuperFood')
+    expect(page).to have_content('TechInd')
+  end
 end

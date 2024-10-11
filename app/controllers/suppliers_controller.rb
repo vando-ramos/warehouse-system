ec052eea7cceb12ff6ad09f2c9a13adf379fa59d
vando <vandorramos@gmail.com>
2024-10-10 22:59:02 -0300

    User views the suppliers page and sees the registered suppliers

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,5 +1,5 @@
 class SuppliersController < ApplicationController
   def index
-
+    @suppliers = Supplier.all
   end
 end
--- spec/system/suppliers/user_views_suppliers_spec.rb
+++ spec/system/suppliers/user_views_suppliers_spec.rb
@@ -13,4 +13,24 @@ describe 'User views the suppliers page' do
     # assert
     expect(current_path).to eq(suppliers_path)
   end
+
+  it 'and sees the registered suppliers' do
+    # arrange
+    supplier = Supplier.create!(corporate_name: 'Comercial Alimentos S.A.', brand_name: 'SuperFood', registration_number: '12.345.678/0001-90',
+                                address: 'Rua das Palmeiras, 123', city: 'São Paulo', state: 'SP', email: 'contato@superfood.com.br')
+
+    supplier = Supplier.create!(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+    # act
+    visit(root_path)
+    click_on('Suppliers')
+
+    # assert
+    expect(page).to have_content('Suppliers')
+    expect(page).to have_content('SuperFood')
+    expect(page).to have_content('São Paulo - SP')
+    expect(page).to have_content('TechInd')
+    expect(page).to have_content('Curitiba - PR')
+  end
 end

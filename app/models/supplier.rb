ee75e376634fd4bac0fd0fd1ae46dbfe0263276d
vando <vandorramos@gmail.com>
2024-10-11 13:17:27 -0300

    Supplier model: attributes can't be blank, corporate name and registration number can be uniqueness

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,5 @@
 class Supplier < ApplicationRecord
   validates :corporate_name, :brand_name, :registration_number, :address, :city, :state, :email, presence: true
+  validates :corporate_name, uniqueness: true
+  validates :registration_number, uniqueness: true
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,112 @@
 require 'rails_helper'
 
 RSpec.describe Supplier, type: :model do
-  
+  describe '#valid?' do
+    context 'presence' do
+      it 'false when the corporate name is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: '', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+
+      it 'false when the brand name is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: '', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+
+      it 'false when the registration number is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+
+      it 'false when the address is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: '', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+
+      it 'false when the city is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: '', state: 'PR', email: 'vendas@techind.com')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+
+      it 'false when the state is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: '', email: 'vendas@techind.com')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+
+      it 'false when the email is empty' do
+        # arrange
+        supplier = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: '')
+
+        # act
+
+        # assert
+        expect(supplier.valid?).to eq false
+      end
+    end
+
+    it 'false when the corporate name is already in use' do
+      # arrange
+      supplier1 = Supplier.create!(corporate_name: 'Comercial Alimentos S.A.', brand_name: 'SuperFood', registration_number: '12.345.678/0001-90',
+                                   address: 'Rua das Palmeiras, 123', city: 'São Paulo', state: 'SP', email: 'contato@superfood.com.br')
+
+      supplier2 = Supplier.new(corporate_name: 'Comercial Alimentos S.A.', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                               address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+      # act
+
+      # assert
+      expect(supplier2.valid?).to eq false
+    end
+
+    it 'false when the registration number is already in use' do
+      # arrange
+      supplier1 = Supplier.create!(corporate_name: 'Comercial Alimentos S.A.', brand_name: 'SuperFood', registration_number: '12.345.678/0001-90',
+                                   address: 'Rua das Palmeiras, 123', city: 'São Paulo', state: 'SP', email: 'contato@superfood.com.br')
+
+      supplier2 = Supplier.new(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '12.345.678/0001-90',
+                               address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+      # act
+
+      # assert
+      expect(supplier2.valid?).to eq false
+    end
+  end
 end

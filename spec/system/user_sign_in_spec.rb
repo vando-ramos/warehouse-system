73dab9d133858ef3e721fe3fb9bda205b14cfc00
vando <vandorramos@gmail.com>
2024-10-13 15:35:58 -0300

    User authenticates and sign out

--- spec/system/user_sign_in_spec.rb
+++ spec/system/user_sign_in_spec.rb
@@ -24,4 +24,31 @@ describe 'User authenticates' do
     end
     expect(page).to have_content('Signed in successfully')
   end
+
+  it 'and log out' do
+    # arrange
+    User.create!(email: 'user@email.com', password: '123456')
+
+    # act
+    visit(root_path)
+    within('nav') do
+      click_on('Sign in')
+    end
+    within('form') do
+      fill_in 'Email', with: 'user@email.com'
+      fill_in 'Password', with: '123456'
+      click_on('Sign in')
+    end
+    within('nav') do
+      click_on('Sign out')
+    end
+
+    # assert
+    within('nav') do
+      expect(page).to have_link('Sign in')
+      expect(page).not_to have_button('Sign out')
+      expect(page).not_to have_content('user@email.com')
+    end
+    expect(page).to have_content('Signed out successfully')
+  end
 end

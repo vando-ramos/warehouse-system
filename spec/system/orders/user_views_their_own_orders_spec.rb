6be6b8a1fb51c0d2597ba31d945d3c21d8269d8f
vando <vandorramos@gmail.com>
2024-10-16 16:57:40 -0300

    User views only their own orders and sees details of an order

--- spec/system/orders/user_views_their_own_orders_spec.rb
+++ spec/system/orders/user_views_their_own_orders_spec.rb
@@ -41,8 +41,36 @@ describe 'User views only their own orders' do
     expect(page).to have_content(order1.code)
     expect(page).to have_content(order2.code)
     expect(page).to have_content('User - user@email.com')
-    
+
     expect(page).not_to have_content(order3.code)
     expect(page).not_to have_content('Admin - admin@email.com')
+  end
+
+  it 'and sees details of an order' do
+    # Arrange
+    user = User.create!(name: 'User', email: 'user@email.com', password: '123456')
+
+    warehouse = Warehouse.create!(name: 'Galeão', code: 'GIG', city: 'Rio de Janeiro', area: 18_000_000, address: 'Av. Jornalista Roberto Marinho, s/n',
+                                  cep: '21941-900' , description: 'Cargas internacionais')
+
+    supplier = Supplier.create!(corporate_name: 'Tecnologia Industrial LTDA', brand_name: 'TechInd', registration_number: '98.765.432/0001-10',
+                                address: 'Avenida das Nações, 456', city: 'Curitiba', state: 'PR', email: 'vendas@techind.com')
+
+    order = Order.create!(user: user, warehouse: warehouse, supplier: supplier, expected_delivery_date: 1.day.from_now.to_date)
+
+    # Act
+    login_as(user)
+    visit(root_path)
+    within('nav') do
+      click_on('Orders')
+    end
+    click_on(order.code)
+
+    # Assert
+    expect(page).to have_content("Order #{order.code}")
+    expect(page).to have_content('User - user@email.com')
+    expect(page).to have_content('Galeão - GIG')
+    expect(page).to have_content('Tecnologia Industrial LTDA')
+    expect(page).to have_content(1.day.from_now.to_date)
   end
 end
